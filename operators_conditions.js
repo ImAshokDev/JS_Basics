75b2d056680cbe2ebb3b8ccbb72046ff1cff63a4
krist yohan <massashok420@gmail.com>
2019-11-28 20:06:01 +0530

    operators

---
+++ operators_conditions.js
@@ -0,0 +1,38 @@
+console.log(6 + 9);
+console.log('khrist' + ' yohan');
+
+console.log(9 - 2);
+console.log(8 * 2);
+console.log(9 / 3);
+
+var numOne = 1;
+var numTwo = 2;
+
+if (numOne === 2) {
+  console.log(numOne);
+} else {
+  console.log(numTwo);
+}
+
+if (numTwo !== 1) {
+  console.log(numTwo);
+}
+var userName = 'Khrist Yohan';
+if (userName === 'Khrist Yohan') {
+  console.log('Loggeg in User : ' + userName);
+} else {
+  console.log('No User');
+}
+
+var name = 'khrish';
+console.log(name);
+if (name == 'khrish') {
+  name += ' :)';
+  console.log(name);
+} else if (name == 'yohan') {
+  name += ' :P';
+  console.log(name);
+} else {
+  name += ' :D';
+  console.log(name);
+}
