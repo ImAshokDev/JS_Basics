c233277cf3eb3367027b4f9846dfc211e326bf69
krist yohan <massashok420@gmail.com>
2019-11-28 18:48:54 +0530

    variables added

---
+++ basics.js
@@ -0,0 +1,27 @@
+// wednasday 28/11/2019 06:43 PM
+
+// Types
+// Number
+// String
+// Boolean
+// Symbol (ES2015)
+// Object
+// Function
+// Array
+// Date
+// RegExp
+// null
+// undefined
+
+// Variables
+var userName;
+console.log(userName); // undefined
+
+userName = 'Khrist';
+console.log(userName);
+
+var username = 'yohan';
+console.log(username);
+
+userName = 'khrist yohan';
+console.log(userName);
