ded42586daca2d06bfc2e3a50cdeac336b1c13da
krist yohan <massashok420@gmail.com>
2019-11-28 18:59:11 +0530

    Changing datatype of variable by changing value

--- basics.js
+++ basics.js
@@ -46,3 +46,22 @@ var userDetails = ['khrist', 'yohan', 27, true, 'Vellore'];
 for (var i = 0; i < userDetails.length; i++) {
   console.log(userDetails[i]);
 }
+
+// Changing datatype of variable by changing value
+var me = 'khrist yohan';
+console.log(me);
+me = 27;
+console.log(me);
+me = false;
+console.log(me);
+me = ['khrist', 'yohan'];
+console.log(me);
+
+// Object
+/*
+the querySelector can only be used when script running on browser
+using query selector to select h1 html tag
+storing the selected object in myVariable
+*/
+
+// var myVariable = document.querySelector('h1');
